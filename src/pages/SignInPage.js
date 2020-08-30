bf265def3345624590afd2bcba519f6e721a0fd2
dragonfist453 <thermosphere453@gmail.com>
2020-08-30 21:35:08 +0530

    Remove unneccessary dependencies. Modify signin page.

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import SignIn from '../components/SignIn';
 
 // Example page for now only containing header
 export default function SignInPage(props) {
     return(
         <React.Fragment>
-            <Header/>
             <SignIn/>
-            <Footer/>
         </React.Fragment>
     )
 }
