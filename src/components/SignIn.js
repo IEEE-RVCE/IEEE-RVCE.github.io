8b4430a96b1ecc824a4a099facdc7d8a23d157ff
Atreya Bain <christophervrose@gmail.com>
2020-08-31 13:23:52 +0530

    Make highlight changes

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,54 +1,85 @@
 import React from 'react';
 import 'tachyons';
 
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       signInEmail: '',
-      signInPassword: ''
+      signInPassword: '',
+      ieeeIDValid: true,
+      passValid: true
     }
   }
 
-  // For further usage if necessary
-  // loadUser = (data) => {
-  //   this.setState({user: {
-  //     id: data.id,
-  //     name: data.name,
-  //     email: data.email,
-  //     entries: data.entries,
-  //     joined: data.joined
-  //   }})
-  // }
+  /**
+   * Validate an email
+   * @param {string} ieeeid 
+   * @returns {boolean}
+   */
+  validateID(ieeeid) {
+    const re = /\d{10}/;
+    return re.test(String(ieeeid).toLowerCase());
+  }
 
-  // For routing
-  // onRouteChange = (route) => {
-  //   if (route === 'signout') {
-  //     this.setState({isSignedIn: false})
-  //   } else if (route === 'home') {
-  //     this.setState({isSignedIn: true})
-  //   }
-  //   this.setState({route: route});
-  // }
+  /**
+   * Validate a password
+   * @param {string} password 
+   * @param {boolean}
+   */
+  validatePassword(password) {
+    const re = /.{7}.+/;
+    return re.test(String(password).toLowerCase());
+  }
 
-  onEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
+  /**
+   * Get classes for email
+   * @param {boolean} state
+   * @returns {string}
+   */
+  getInputClasses(state) {
+    let base = 'pa2 input-reset ba bg-transparent w-100';
+    if (!state) {
+      base += ' b--red';
+    }
+    return base;
+  }
+
+
+
+  onIDChange = (event) => {
+    console.warn(this.validateID(event.target.value)); 
+    this.setState({
+      signInEmail: event.target.value,
+      ieeeIDValid: this.validateID(event.target.value)
+    })
   }
 
   onPasswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
+    this.setState({
+      signInPassword: event.target.value,
+      passValid: this.validatePassword(event.target.value)
+    })
+
   }
 
-  onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
+  /**
+   * 
+   * @param {React.MouseEvent<HTMLInputElement, MouseEvent>} event 
+   */
+  onSubmitSignIn = (event) => {
+    // Put some assertion code to submit only if 
+    // ids & pass are valid
+    fetch('http://localhost:3000/api/auth', {
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.signInEmail,
         password: this.state.signInPassword
       })
     })
-      .then(response => response.json())
+      .then(response => response.json(),(rejectionreason)=>console.error)
       .then(user => {
         if (user.id) {
           // To set the state of various state variables upon getting the response
@@ -69,25 +100,27 @@ class SignIn extends React.Component {
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className={this.getInputClasses(this.state.ieeeIDValid)}
                   type="email"
                   name="email-address"
                   id="email-address"
-                  onChange={this.onEmailChange}
+                  placeholder="IEEE ID"
+                  onChange={this.onIDChange}
                 />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className={this.getInputClasses(this.state.passValid)}
                   type="password"
                   name="password"
                   id="password"
+                  placeholder="Password"
                   onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
-            <div className="">
+            <div className="submitContainer">
               <input
                 onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
