c30bb2fe8a579cf0913abcaa3cc34354b3ecd2b5
dragonfist453 <thermosphere453@gmail.com>
2020-11-26 23:26:24 +0530

    Auto scroll to top on navigation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router} from 'react-router-dom';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
   <Router basename='/'>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </Router>,
   document.getElementById('root')
 );
