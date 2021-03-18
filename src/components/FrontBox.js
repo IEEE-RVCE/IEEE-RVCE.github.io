a1cabeaf6bc7cc4b3bcf3d502b5a5f7300f9841c
Atreya Bain <christophervrose@gmail.com>
2021-03-18 21:39:57 +0530

    [add] background image (placeholder)

--- src/components/FrontBox.js
+++ src/components/FrontBox.js
@@ -2,27 +2,41 @@ import { Box, Grid, makeStyles, Typography,Button } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles(theme=>({
-    tell:{
-        color:'#333'
+    bigbutton:{
+        padding:'.6rem 1.2rem'
+    },
+    bigbadBackground:{
+        backgroundImage:'url(assets/images/devs/Chirag_Bapat.jpg)',
+        backgroundSize:'cover',
+        backgroundPosition:'center',
+        backgroundRepeat:'no-repeat'
     }
 }));
 export default function FrontBox(){
     const classes = useStyles();
     return (
-        <Box >
-            <Typography variant='h4' >IEEE RVCE</Typography>
+        <>
+        <Box className={classes.bigbadBackground}>
+            <Grid container style={{padding:'6rem'}} alignContent='center' justify='center'>
+                <Grid item>
+                    <Typography variant='h4' >IEEE RVCE</Typography>
+
+                </Grid>
+            </Grid>
             <br />
 
             <Grid container alignContent='center' alignItems='center' direction='row' justify='center' spacing={2}>
                 <Grid item>
-                <Button variant='outlined' size='large'>Something</Button>
+                <Button className={classes.bigbutton} variant='outlined' size='large'>Something</Button>
 
                 </Grid>
                 <Grid item>
-                    <Button variant='contained' color="primary" size='large'>Join US</Button>
+                    <Button className={classes.bigbutton} variant='contained' color="primary" size='large'>Join US</Button>
                 </Grid>
             </Grid>
             <br />
         </Box>
+        <br />
+        </>
     );
 }
