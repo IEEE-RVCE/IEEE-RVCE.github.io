716c29e9cb902b2a2aa72155f81384cdf3ebf43e
Atreya Bain <christophervrose@gmail.com>
2021-03-24 17:17:41 +0530

    [trim] css

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Carousel } from "react-responsive-carousel";
 import { makeStyles } from "@material-ui/core";
