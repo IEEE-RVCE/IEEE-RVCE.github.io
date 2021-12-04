95c1cf72f5f99df4891a327e67a6655108539fa6
Ambu Karthik <thermosphere453@gmail.com>
2021-12-04 12:22:39 +0530

    Revert "[Fix] Fixed Event Sorting closes #29 in site-rear"

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -6,35 +6,34 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import Button from "@material-ui/core/Button";  
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 const useStyles = makeStyles((theme) => ({
-  "@global": {
-    ".MuiCardHeader-content": {
-      minWidth: 0,
-    },
+  '@global': {
+    '.MuiCardHeader-content': {
+      minWidth: 0
+    }
   },
   root: {
     ...theme.eventcard,
-    height: "100%",
+    height: '100%',
     maxWidth: 345,
-    "&:hover": {
-      boxShadow:
-        "0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)",
+    '&:hover': {
+      boxShadow: "0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)",
       transitionDuration: 200,
     },
-    margin: "auto",
+    margin: 'auto',
   },
   media: {
     height: 0,
-    paddingTop: "56.25%", // 16:9
+    paddingTop: "56.25%" // 16:9
   },
 }));
 
@@ -44,15 +43,13 @@ export default function EventCard(props) {
 
   const eventTimes = {
     start: new Date(props.event.eventstart),
-    end: new Date(props.event.eventend),
-  };
+    end: new Date(props.event.eventend)
+  }
 
   //Shows snackbar with message and copies link in '' for user
   const handleClick = () => {
     setOpen(true);
-    navigator.clipboard.writeText(
-      window.location.origin + "/#/events/" + props.event.eid
-    );
+    navigator.clipboard.writeText(window.location.origin + '/#/events/' + props.event.eid);
   };
 
   const handleClose = (event, reason) => {
@@ -65,20 +62,10 @@ export default function EventCard(props) {
 
   return (
     <>
-      <Card
-        title={props.event.ename}
-        className={classes.root}
-        onDoubleClick={() =>
-          (window.location.href = window.location.href + "/" + props.event.eid)
-        }
-      >
+      <Card title={props.event.ename} className={classes.root} onDoubleClick={() => window.location.href = window.location.href + '/' + props.event.eid}>
         <CardHeader
           title={props.event.ename}
-          subheader={
-            eventTimes.start.toString().slice(4, 15) +
-            " to " +
-            eventTimes.end.toString().slice(4, 15)
-          }
+          subheader={eventTimes.start.toString().slice(4,15) + " to " + eventTimes.end.toString().slice(4,15)}
           titleTypographyProps={{
             noWrap: true,
           }}
@@ -89,16 +76,11 @@ export default function EventCard(props) {
           title={props.event.ename}
         />
         <CardContent>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"
-            noWrap
-          >
+          <Typography variant="body2" color="textSecondary" component="p" noWrap>
             Keywords: {props.event.keywords}
           </Typography>
         </CardContent>
-        <CardActions disableSpacing style={{ marginTop: "auto" }}>
+        <CardActions disableSpacing style={{marginTop:"auto"}}>
           <Button size="small" onClick={handleClick}>
             Share
           </Button>
@@ -107,10 +89,7 @@ export default function EventCard(props) {
               Link Copied!
             </Alert>
           </Snackbar>
-          <Link
-            style={{ textDecoration: "none" }}
-            to={"/events/" + props.event.eid}
-          >
+          <Link style={{textDecoration: 'none'}} to={'/events/'+props.event.eid}>
             <Button size="small">Read More</Button>
           </Link>
         </CardActions>
--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,257 +1,199 @@
-/* eslint-disable no-lone-blocks */
-import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  Typography,
-  Grid,
-  TextField,
-  MenuItem,
-  Select,
-  InputLabel,
-  FormControl,
-  Container,
-  Fab,
-  Tooltip,
-} from "@material-ui/core";
-import EventCard from "../components/EventCard";
-import axios from "axios";
-import { ecats, hostname } from "../links";
-import { Skeleton } from "@material-ui/lab";
-import { Add } from "@material-ui/icons";
-import { AddEventDialog } from "../components/AddEventDialog";
-import * as queryString from "query-string";
-import EventsBox from "../components/EventsBox";
-import GiveMeABreak from "../components/GiveMeABreak";
+import React,{useState,useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Grid, TextField, MenuItem, Select, InputLabel, FormControl, Container, Fab, Tooltip} from '@material-ui/core';
+import EventCard from '../components/EventCard';
+import axios from 'axios';
+import { ecats, hostname } from '../links';
+import {Skeleton} from '@material-ui/lab';
+import { Add } from '@material-ui/icons';
+import { AddEventDialog } from '../components/AddEventDialog';
+import * as queryString from 'query-string';
+import EventsBox from '../components/EventsBox';
+import GiveMeABreak from '../components/GiveMeABreak';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    // ...theme.page,
-    ...theme.root,
-    textAlign: "center",
-    alignContent: "center",
-  },
-  instructions: {
-    marginLeft: theme.spacing(3),
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-  backdrop: {
-    zIndex: 100,
-    color: "#fff",
-  },
-  fab: {
-    ...theme.fab,
-    right: 100,
-  },
-  searchBars: {
-    display: "block",
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
+    root: {
+        // ...theme.page,
+        ...theme.root,
+        textAlign: 'center',
+        alignContent: 'center',
     },
-  },
-  bar: {
-    ...theme.transbg,
-  },
+    instructions: {
+        marginLeft: theme.spacing(3),
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    },
+    backdrop: {
+        zIndex: 100,
+        color: '#fff',
+    },
+    fab: {
+        ...theme.fab,
+        right: 100,
+    },
+    searchBars: {
+        display: 'block',
+        [theme.breakpoints.down('sm')]: {
+            display: 'none'
+        },
+    },
+    bar: {
+        ...theme.transbg,
+    },
 }));
 
 export default function EventPage(props) {
-  const classes = useStyles();
-  const [loading, setLoading] = React.useState(false);
-  let loggedIn = localStorage.getItem("isAuthenticated") === "true";
-  const { ecat } = queryString.parse(props.location.search);
+    const classes = useStyles();
+    const [loading, setLoading] = React.useState(false)
+    let loggedIn = localStorage.getItem('isAuthenticated') === 'true';
+    const {ecat} = queryString.parse(props.location.search)
 
-  //Get events from db and use a list to manage which events are being shown
-  const [events, setEvents] = useState([]);
-  const [list, setList] = React.useState([
-    { hi: "these" },
-    { dont: "mean" },
-    { anything: "they" },
-    { help: "skeletons" },
-  ]);
+    //Get events from db and use a list to manage which events are being shown
+    const [events, setEvents] = useState([]);
+    const [list, setList] = React.useState([
+        {"hi": "these"},
+        {"dont": "mean"},
+        {"anything": "they"},
+        {"help": "skeletons"}
+    ]);
 
-  //search text
-  const [text, setText] = React.useState("");
-  //Keyword search
-  const handleSearch = (event) => {
-    setText(event.target.value);
-  };
+    //search text
+    const [text, setText] = React.useState("");
+    //Keyword search
+    const handleSearch = (event) => {
+        setText(event.target.value);
+    }
 
-  useEffect(() => {
-    var updatedList = events;
-    updatedList = events.filter(function (item) {
-      if (text === "") return events;
-      return (
-        item.keywords.toLowerCase().search(text.toLowerCase()) !== -1 ||
-        item.ename.toLowerCase().search(text.toLowerCase()) !== -1
-      );
-    });
-    setList(updatedList);
-  }, [events, text]);
+    useEffect(() => {
+        var updatedList = events;
+        updatedList = events.filter(function (item) {
+            if (text === "")
+                return events;
+            return (
+                item.keywords.toLowerCase().search(text.toLowerCase()) !== -1 ||
+                item.ename.toLowerCase().search(text.toLowerCase()) !== -1
+            );
+        });
+        setList(updatedList);
+    },[events, text])
 
-  //Society names for dropdown filter
-  const [category, setCategory] = React.useState(ecat !== undefined ? ecat : 0);
-  //Society filter
-  const handleFilter = (event) => {
-    setCategory(event.target.value);
-    window.location.href =
-      event.target.value !== 0
-        ? window.location.origin + "/#/events?ecat=" + event.target.value
-        : window.location.origin + "/#/events";
-  };
+    //Society names for dropdown filter
+    const [category, setCategory] = React.useState(ecat !==undefined?ecat:0);
+    //Society filter
+    const handleFilter = (event) => {
+        setCategory(event.target.value);
+        window.location.href = event.target.value!==0?window.location.origin + '/#/events?ecat=' + event.target.value:window.location.origin + '/#/events';
+    }
 
-  //Dialog stuff
-  const [dialog, setDialog] = useState(false);
-  const handleDialogClose = () => {
-    setDialog(false);
-  };
-  const handleDialogOpen = () => {
-    setDialog(true);
-  };
+    //Dialog stuff
+    const [dialog, setDialog] = useState(false);
+    const handleDialogClose = () => {
+        setDialog(false)
+    }
+    const handleDialogOpen = () => {
+        setDialog(true)
+    }
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(
-        ecat !== undefined
-          ? hostname + "/api/event/cat/" + ecat
-          : hostname + "/api/event"
-      )
-      .then((response) => {
-        setEvents(response.data.events);
-        setList(response.data.events);
-      })
-      .then(() => {
-        setLoading(false);
-      });
-  }, [ecat]);
-
-  // Event Sorted on basis of Event Start Date; (if anyone has a better idea, let's goooo!)
-  list.sort(function (a, b) {
-    return new Date(b.eventstart) - new Date(a.eventstart);
-  });
-  return (
-    <div className={classes.root}>
-      <EventsBox />
-      <Container maxWidth="lg">
-        <div className={classes.searchBars}>
-          <div
-            className={classes.bar}
-            style={{ float: "right", display: "flex", flexDirection: "row" }}
-          >
-            <FormControl>
-              <TextField
-                value={text}
-                onChange={handleSearch}
-                placeholder="Enter keywords or name"
-                label="Search events"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                defaultValue="All"
-              />
-            </FormControl>
-          </div>
-          <div
-            className={classes.bar}
-            style={{
-              float: "left",
-              display: "flex",
-              flexDirection: "row-reverse",
-            }}
-          >
-            <FormControl>
-              <InputLabel id="ecat-search-label">Search by category</InputLabel>
-              <Select
-                labelId="ecat-search-label"
-                id="ecat-search"
-                value={category}
-                onChange={handleFilter}
-                style={{ minWidth: "200px" }}
-              >
-                <MenuItem key={"All"} value={0}>
-                  All
-                </MenuItem>
-                <MenuItem key={"CompSoc"} value={ecats.compsoc}>
-                  Computer Society
-                </MenuItem>
-                <MenuItem key={"ComSoc"} value={ecats.comsoc}>
-                  Communication Society
-                </MenuItem>
-                <MenuItem key={"APS"} value={ecats.aps}>
-                  Antenna Propogation Society
-                </MenuItem>
-                <MenuItem key={"SPS"} value={ecats.sps}>
-                  Signal Processing Society
-                </MenuItem>
-                <MenuItem key={"PES"} value={ecats.pes}>
-                  Power and Energy Society
-                </MenuItem>
-                <MenuItem key={"RAS"} value={ecats.ras}>
-                  Robotic and Automation Society
-                </MenuItem>
-                <MenuItem key={"SIGHT"} value={ecats.sight}>
-                  Special Interest Group on Humanitarian Technology
-                </MenuItem>
-                <MenuItem key={"WIE"} value={ecats.wie}>
-                  Women in Engineering
-                </MenuItem>
-              </Select>
-            </FormControl>
-          </div>
+    useEffect(() => {
+        setLoading(true)
+        axios.get(ecat!==undefined?hostname+'/api/event/cat/'+ecat:hostname+'/api/event')
+            .then(response => {
+                setEvents(response.data.events)
+                setList(response.data.events)
+            })
+            .then(() => {
+                setLoading(false)
+            })
+    }, [ecat]);
+    return (
+            <div className={classes.root}>
+                <EventsBox/>
+                <Container maxWidth='lg'>
+                    <div className={classes.searchBars}>
+                        <div className={classes.bar} style={{ float: "right", display: "flex", flexDirection: 'row', }}>
+                            <FormControl>
+                                <TextField
+                                    value={text}
+                                    onChange={handleSearch}
+                                    placeholder="Enter keywords or name"
+                                    label="Search events"
+                                    InputLabelProps= {{
+                                        shrink: true
+                                    }}
+                                    defaultValue='All'
+                                />
+                            </FormControl>
+                        </div>
+                        <div className={classes.bar} style={{ float: "left", display: "flex", flexDirection: 'row-reverse', }}>
+                            <FormControl>
+                                <InputLabel id='ecat-search-label'>Search by category</InputLabel>
+                                <Select
+                                    labelId='ecat-search-label'
+                                    id='ecat-search'
+                                    value={category}
+                                    onChange={handleFilter}
+                                    style={{minWidth:"200px"}}
+                                >
+                                    <MenuItem key={"All"} value={0}>All</MenuItem>
+                                    <MenuItem key={"CompSoc"} value={ecats.compsoc}>Computer Society</MenuItem>
+                                    <MenuItem key={"ComSoc"} value={ecats.comsoc}>Communication Society</MenuItem>
+                                    <MenuItem key={"APS"} value={ecats.aps}>Antenna Propogation Society</MenuItem>
+                                    <MenuItem key={"SPS"} value={ecats.sps}>Signal Processing Society</MenuItem>
+                                    <MenuItem key={"PES"} value={ecats.pes}>Power and Energy Society</MenuItem>
+                                    <MenuItem key={"RAS"} value={ecats.ras}>Robotic and Automation Society</MenuItem>
+                                    <MenuItem key={"SIGHT"} value={ecats.sight}>Special Interest Group on Humanitarian Technology</MenuItem>
+                                    <MenuItem key={"WIE"} value={ecats.wie}>Women in Engineering</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
+                    </div>
+                    {/* <Typography variant='h4' style={{textAlign: 'center'}}><b>Events</b></Typography> */}
+                    <br/>
+                    {
+                        (Array.isArray(list) && list.length!==0)?(
+                            <Grid container spacing={3} direction='row' alignItems="stretch" className={classes.grid}>
+                                {list.map(function (item) {
+                                    if(loading)
+                                    {
+                                        return(
+                                            <Grid item lg={3} sm={6} xs={12} md={4}>
+                                                <Skeleton animation="wave" variant="rect" height={400}/>
+                                            </Grid>
+                                        )
+                                    }
+                                    else
+                                    {
+                                        return(   
+                                            <Grid item lg={3} sm={6} xs={12} md={4}>
+                                                <EventCard event={item} />
+                                            </Grid>
+                                        )
+                                    }
+                                })
+                                }
+                            </Grid>
+                        )
+                        :
+                        (
+                            <Typography variant='h5' style={{textAlign: 'center'}}>No events to display</Typography>
+                        )
+                    }
+            </Container>
+            {
+                loggedIn && (
+                    <>
+                        <Tooltip title="Add event" aria-label="add-event-tooltip">
+                            <Fab onClick={handleDialogOpen} aria-label='addEvent' className={classes.fab}>
+                                <Add/>
+                            </Fab>
+                        </Tooltip>
+                        <AddEventDialog onClose={handleDialogClose} aria-label="add-event-dialog" open={dialog}/>
+                    </>
+                )
+            }
+            <GiveMeABreak/>
+            <GiveMeABreak/>
+            <GiveMeABreak/>
         </div>
-        {/* <Typography variant='h4' style={{textAlign: 'center'}}><b>Events</b></Typography> */}
-        <br />
-        {Array.isArray(list) && list.length !== 0 ? (
-          <Grid
-            container
-            spacing={3}
-            direction="row"
-            alignItems="stretch"
-            className={classes.grid}
-          >
-            {list.map(function (item) {
-              if (loading) {
-                return (
-                  <Grid item lg={3} sm={6} xs={12} md={4}>
-                    <Skeleton animation="wave" variant="rect" height={400} />
-                  </Grid>
-                );
-              } else {
-                return (
-                  <Grid item lg={3} sm={6} xs={12} md={4}>
-                    <EventCard event={item} />
-                  </Grid>
-                );
-              }
-            })}
-          </Grid>
-        ) : (
-          <Typography variant="h5" style={{ textAlign: "center" }}>
-            No events to display
-          </Typography>
-        )}
-      </Container>
-      {loggedIn && (
-        <>
-          <Tooltip title="Add event" aria-label="add-event-tooltip">
-            <Fab
-              onClick={handleDialogOpen}
-              aria-label="addEvent"
-              className={classes.fab}
-            >
-              <Add />
-            </Fab>
-          </Tooltip>
-          <AddEventDialog
-            onClose={handleDialogClose}
-            aria-label="add-event-dialog"
-            open={dialog}
-          />
-        </>
-      )}
-      <GiveMeABreak />
-      <GiveMeABreak />
-      <GiveMeABreak />
-    </div>
-  );
+    );
 }
