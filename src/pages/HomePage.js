13815532ea2302b5c8fe56bc9e4235935b98737a
Prajwal P <prajwalprakash3722@gmail.com>
2022-02-05 13:23:40 +0530

    [fix]

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
 import {
