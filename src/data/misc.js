83192ebd9d16ba6aaf622e90e3cefecba6bffeab
Prajwal P <prajwalprakash3722@gmail.com>
2022-02-08 00:03:14 +0530

    [update] Host Change

--- src/data/misc.js
+++ src/data/misc.js
@@ -1,17 +1,17 @@
 //@ts-check
 /** Hostname link for API requests */
-export const hostname = "https://play.kekvrose.me";
+export const hostname = "https://ieee.devcoffee.me";
 /** Image source links */
-export const imagePath = '/assets/images';
+export const imagePath = "/assets/images";
 
 /**
  * Template function to add imagepath to string (Beginning slash added automatically)
  * Eg. imgify\`x\` will result in imagePath+'x'
- * @param {TemplateStringsArray} x 
+ * @param {TemplateStringsArray} x
  */
-export function imgify(x){
-    // dont put slash ifn't needed
-    const optionalSlash = x[0][0]==='/'?'':'/';
-    // join up x and add everything in
-    return imagePath+optionalSlash+x.join('');
+export function imgify(x) {
+  // dont put slash ifn't needed
+  const optionalSlash = x[0][0] === "/" ? "" : "/";
+  // join up x and add everything in
+  return imagePath + optionalSlash + x.join("");
 }
