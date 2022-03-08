153bdd2049c52d4ac474a8d8305199595733b03b
Prajwal P <prajwalprakash3722@gmail.com>
2022-03-08 17:11:48 +0530

    [update] MemberShip Costs Update

--- src/data/membershipCosts.js
+++ src/data/membershipCosts.js
@@ -1,4 +1,3 @@
-
 export const exchangeRate = 73;
 /**
  * Membership costs. Cost is by default 0, notes by default is taken as empty.
@@ -6,31 +5,41 @@ export const exchangeRate = 73;
  */
 
 export const costs = [
-    {
-        name: 'Student Membership',
-        cost: 27,
-        discountedCost: (27 / 2),
-        notes: 'FUTURE50 (50% off)'
-    },
-    
-    {
-        name: 'Antennas and Propagation Society (APS)', cost: 8,
-        
-    },
+  {
+    name: "Student Membership",
+    cost: 27,
+    discountedCost: 27 / 2,
+    notes: "FUTURE50 (50% off)",
+  },
 
-    {name:'Circuits and Systems (CAS)', cost:11},
-    
-    { name: 'Communications Society', cost: 1 },
-    
-    { name: 'Computer Society', cost: 8 ,discountedCost:4,notes:'CS202250 (50% off)'},
+  {
+    name: "Antennas and Propagation Society (APS)",
+    cost: 4,
+  },
 
-    { name: 'Power & Energy Society (PES)', cost: 15 ,discountedCost:0,notes:'Free for first year!'},
-        
-    { name: 'Robotics and Automation (RAS)', cost: 5 },
-    
-    { name: 'Signal Processing Society (SPS)', cost: 11, },
+  { name: "Circuits and Systems (CAS)", cost: 9 },
 
-    { name: 'SIGHT' ,cost:0},
+  { name: "Communications Society", cost: 0.5 },
 
-    { name: 'Women in Engineering (WIE)',cost:0 }
+  {
+    name: "Computer Society",
+    cost: 4,
+    // discountedCost: 4,
+    notes: "CS202250 (50% off)",
+  },
+
+  {
+    name: "Power & Energy Society (PES)",
+    cost: 9,
+    // discountedCost: 0,
+    notes: "Free for first year!",
+  },
+
+  { name: "Robotics and Automation (RAS)", cost: 2.5 },
+
+  { name: "Signal Processing Society (SPS)", cost: 10 },
+
+  { name: "SIGHT", cost: 0 },
+
+  { name: "Women in Engineering (WIE)", cost: 0 },
 ];
