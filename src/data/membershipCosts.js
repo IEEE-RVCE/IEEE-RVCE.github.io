18a376e1b521ec15c8a647299abdb9f7230a3acc
naveenbteli <naveenbteli.24@gmail.com>
2022-12-10 22:35:27 +0530

    membership prizes changed

--- src/data/membershipCosts.js
+++ src/data/membershipCosts.js
@@ -1,4 +1,4 @@
-export const exchangeRate = 73;
+export const exchangeRate = 1;
 /**
  * Membership costs. Cost is by default 0, notes by default is taken as empty.
  * @type {{name:string,cost?:number,discountedCost?:number,notes?:string}[]}
@@ -7,37 +7,39 @@ export const exchangeRate = 73;
 export const costs = [
   {
     name: "Student Membership",
-    cost: 27,
-    discountedCost: 27 / 2,
-    notes: "FUTURE50 (50% off)",
+    cost: 1078,
+   // discountedCost: 27 / 2,
+    //notes: "FUTURE50 (50% off)",
   },
 
   {
     name: "Antennas and Propagation Society (APS)",
-    cost: 4,
+    cost: 616,
   },
 
-  { name: "Circuits and Systems (CAS)", cost: 9 },
+  { name: "Circuits and Systems (CAS)", cost: 847 },
 
-  { name: "Communications Society", cost: 0.5 },
+  { name: "Communications Society", cost: 38.5 },
 
   {
     name: "Computer Society",
-    cost: 4,
+    cost: 308,
     // discountedCost: 4,
-    notes: "CS202250 (50% off)",
+   // notes: "CS202250 (50% off)",
   },
 
   {
     name: "Power & Energy Society (PES)",
-    cost: 9,
+    cost: 577.5,
     // discountedCost: 0,
     notes: "Free for first year!",
   },
 
-  { name: "Robotics and Automation (RAS)", cost: 2.5 },
+  { name: "Robotics and Automation (RAS)", cost: 385 },
 
-  { name: "Signal Processing Society (SPS)", cost: 10 },
+  { name: "Sensor Council", cost: 0 },
+
+  { name: "Signal Processing Society (SPS)", cost: 38.5 },
 
   { name: "SIGHT", cost: 0 },
 
