338829cae71a8e45588c6acdc388c3d86ee57230
Ambu, Karthik <karthik.ambu@boeing.com>
2023-03-20 14:04:40 +0530

    Added narrower type in docstring

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 /**
  * Converts date values in an object to UTC, based on list of keys provided
- * @param {*} object Input object to process
- * @param {*} keys Keys that have date values in object
+ * @param {Object} object Input object to process
+ * @param {String[]} keys Keys that have date values in object
  * @returns New object with the values of the keys converted to UTC
  */
 export function convertDatesinValuestoUTC(object, keys) {
