b00d4defaf9fb3e748f07e78cba46478e91e7ba2
Prajwal P <prajwalprakash3722@gmail.com>
2023-03-26 10:40:09 +0530

    fix: added more accurate type infering in convertDatesinValuestoUTC method

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,16 @@
 /**
  * Converts date values in an object to UTC, based on list of keys provided
- * @param {Object} object Input object to process
- * @param {String[]} keys Keys that have date values in object
+ * @template T
+ * @param {T} object Input object to process
+ * @param {(keyof T)[]} keys Keys that have date values in object
  * @returns New object with the values of the keys converted to UTC
  */
 export function convertDatesinValuestoUTC(object, keys) {
-    const result = { ...object };
-    keys.forEach((key) => {
-        if (result[key]) {
-            result[key] = new Date(result[key]).toISOString();
-        }
-    });
-    return result;
+  const result = { ...object };
+  keys.forEach(key => {
+    if (result[key]) {
+      result[key] = new Date(result[key]).toISOString();
+    }
+  });
+  return result;
 }
