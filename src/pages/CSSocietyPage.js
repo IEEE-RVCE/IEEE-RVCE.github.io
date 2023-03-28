444c6576cc562a57fcdfb4c50f7081d7845ce9d0
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2023-03-29 00:45:00 +0530

    added delete execom feature

--- src/pages/CSSocietyPage.js
+++ src/pages/CSSocietyPage.js
@@ -5,6 +5,8 @@ import { Container,
   Typography, 
   Paper,
   Fab,
+  IconButton,
+  Snackbar,
   Tooltip } from '@material-ui/core';
   import { Add } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
@@ -15,6 +17,7 @@ import { hostname, ecats, images, execom, alumni } from '../links';
 import AlumniAccordions from '../components/AlumniAccordions';
 import SpacyDivider from '../components/SpacyDivider';
 import { AddExecomDialog } from '../components/AddExecomDialog';
+import Alert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles(theme => ({
   root: theme.root,
@@ -62,6 +65,38 @@ export default function CSSocietyPage(props) {
       );
     });
   }, []);
+
+  const [del, setDel] = useState({
+    error: false,
+    success: false,
+  });
+
+  const handleClose = prop => (event, reason) => {
+    if (reason === 'clickaway') return;
+    setDel({ ...del, [prop]: false });
+  };
+
+  const deleteExecom = () => {
+    // console.log(hostname + '/api/execom/end/'+ ecats.compsoc);
+    axios
+      .post(hostname + '/api/execom/end/', ecats.compsoc, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('atoken'),
+        },
+    })
+    .then(response => {
+      if (response.data.ok === true) setDel({ ...del, success: true });
+      else setDel({ ...del, error: true });
+    })
+    .then(() => {
+      window.location.reload();
+    })
+    .catch(error => {
+      console.error(error.response);
+      setDel({ ...del, error: true });
+    });
+  }
 
   return (
     <div className={classes.root}>
@@ -127,7 +162,14 @@ export default function CSSocietyPage(props) {
           </>
         )}
         <Paper className={classes.paper}>
-          <Typography variant="h3">Executive Committee</Typography>
+          <Typography variant="h3">Executive Committee
+          <IconButton component="span" onClick={deleteExecom}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
+          <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
+          <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
+          </svg>
+          </IconButton>
+          </Typography>
           <br />
           <Grid container spacing={2} justify="center">
             {execom.compsoc.map(member => (
@@ -150,6 +192,16 @@ export default function CSSocietyPage(props) {
         <AlumniAccordions members={alumni.compsoc} color="rgb(80 161 99)" />
         <br />
       </Container>
+      <Snackbar open={del.error} autoHideDuration={6000} onClose={handleClose('error')}>
+        <Alert elevation={6} variant="filled" onClose={handleClose('error')} severity="error">
+          An error occurred while deleting, please try again
+        </Alert>
+      </Snackbar>
+      <Snackbar open={del.success} autoHideDuration={6000} onClose={handleClose('success')}>
+        <Alert elevation={6} variant="filled" onClose={handleClose('success')} severity="success">
+          Successfully ended tenure of current execom
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
