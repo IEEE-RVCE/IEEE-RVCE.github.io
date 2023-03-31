b62a174cfbac1ad21c6b13ba37f344b45814c5c3
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2023-03-31 16:37:40 +0530

    moved repetitive code

---
+++ src/components/SocietyExecom.js
@@ -0,0 +1,131 @@
+import axios from 'axios';
+import { hostname, ecats} from '../links';
+import React, {useState} from 'react';
+import Alert from '@material-ui/lab/Alert';
+import {  Snackbar,
+    Grid,
+    Tooltip,
+    Fab } from '@material-ui/core';
+import { Add,School}from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '../components/Avatar';
+import { AddExecomDialog } from './AddExecomDialog';
+
+const useStyles = makeStyles(theme => ({
+    root: theme.root,
+    container: theme.page,
+    paper: {
+      ...theme.paper,
+      padding: theme.spacing(4),
+    },
+    link: {
+      ...theme.link,
+      float: 'right',
+      textDecoration: 'none',
+    },
+    carousel: {
+      margin: 'auto',
+      paddingTop: theme.spacing(4),
+    },
+    grid: theme.grid,
+    fab: {
+      color:"green",
+    }
+  }));
+
+export default function SocietyExecom(props) {
+    const exelist = props.exelist;
+    const sid = props.sid;
+
+    let loggedIn = localStorage.getItem('isAuthenticated') === 'true';
+    const classes = useStyles();
+
+    //Dialog stuff
+    const [dialog, setDialog] = useState(false);
+    const handleDialogClose = () => {
+      setDialog(false);
+    };
+    const handleDialogOpen = () => {
+      setDialog(true);
+    };
+  
+    const [del, setDel] = useState({
+        error: false,
+        success: false,
+      });
+
+    const handleClose = prop => (event, reason) => {
+        if (reason === 'clickaway') return;
+        setDel({ ...del, [prop]: false });
+      };
+    
+    const deleteExecom = () => {
+    console.log(hostname + '/api/execom/end/'+ ecats.compsoc);
+    axios
+      .post(hostname + '/api/execom/end/', sid, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('atoken'),
+        },
+    })
+    .then(response => {
+      if (response.data.ok === true) setDel({ ...del, success: true });
+      else setDel({ ...del, error: true });
+    })
+    .then(() => {
+      window.location.reload();
+    })
+    .catch(error => {
+      console.error(error.response);
+      setDel({ ...del, error: true });
+    });
+  }
+
+  return(
+    <>
+    <Grid container spacing={2} justifyContent="center">
+    {exelist.map(member => (
+        <Grid item xs={12} md={4}>
+            <Avatar name={member.name} 
+            position={member.position} 
+            src={member.image} />
+        </Grid>
+        ))}
+    {loggedIn && (
+        <>
+        <Grid item xs={6} container justifyContent="center" alignItems="center">
+        <Tooltip title="Add Execom" arrow placement="top" aria-label="add-execom-tooltip">
+            <span>
+            <Fab onClick={handleDialogOpen} aria-label="addExecom" className={classes.fab}>
+              <Add />
+            </Fab>
+            </span>
+        </Tooltip>
+        <AddExecomDialog onClose={handleDialogClose} aria-label="add-execom-dialog" open={dialog} sid={ecats.compsoc}/>
+        </Grid>
+        <Grid item xs={6} container justifyContent="center" alignItems="center">
+        <Tooltip title="End Tenure" arrow placement="top" aria-label="end-tenure-execom-tooltip">
+            <span>
+            <Fab onClick={deleteExecom} aria-label="endTenureOfExecom" className={classes.fab}>
+            <School />
+            </Fab>
+            </span>
+        </Tooltip>
+        </Grid>
+        </>
+      )}
+    </Grid>
+    <Snackbar open={del.error} autoHideDuration={6000} onClose={handleClose('error')}>
+    <Alert elevation={6} variant="filled" onClose={handleClose('error')} severity="error">
+      An error occurred , please try again
+    </Alert>
+  </Snackbar>
+  <Snackbar open={del.success} autoHideDuration={6000} onClose={handleClose('success')}>
+    <Alert elevation={6} variant="filled" onClose={handleClose('success')} severity="success">
+      Successfully ended tenure of current execom
+    </Alert>
+  </Snackbar>
+  </>
+  );
+
+};
--- src/pages/CSSocietyPage.js
+++ src/pages/CSSocietyPage.js
@@ -3,21 +3,16 @@ import axios from 'axios';
 import { Container,
   Grid, 
   Typography, 
-  Paper,
-  Fab,
-  IconButton,
-  Snackbar,
-  Tooltip } from '@material-ui/core';
-  import { Add } from '@material-ui/icons';
+  Paper} from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '../components/Avatar';
+
 import EventCard from '../components/EventCard';
-import { hostname, ecats, images, execom, alumni } from '../links';
+import { hostname, ecats, images,execom, alumni } from '../links';
 import AlumniAccordions from '../components/AlumniAccordions';
 import SpacyDivider from '../components/SpacyDivider';
-import { AddExecomDialog } from '../components/AddExecomDialog';
-import Alert from '@material-ui/lab/Alert';
+import SocietyExecom from '../components/SocietyExecom';
+
 
 const useStyles = makeStyles(theme => ({
   root: theme.root,
@@ -36,26 +31,12 @@ const useStyles = makeStyles(theme => ({
     paddingTop: theme.spacing(4),
   },
   grid: theme.grid,
-  fab: {
-    color:"green",
-  }
 }));
 
 export default function CSSocietyPage(props) {
   const classes = useStyles();
-  let loggedIn = localStorage.getItem('isAuthenticated') === 'true';
-
   const [events, setEvents] = useState([]);
 
-    //Dialog stuff
-    const [dialog, setDialog] = useState(false);
-    const handleDialogClose = () => {
-      setDialog(false);
-    };
-    const handleDialogOpen = () => {
-      setDialog(true);
-    };
-  
   useEffect(() => {
     axios.get(hostname + '/api/event/cat/' + ecats.compsoc).then(response => {
       setEvents(
@@ -65,38 +46,6 @@ export default function CSSocietyPage(props) {
       );
     });
   }, []);
-
-  const [del, setDel] = useState({
-    error: false,
-    success: false,
-  });
-
-  const handleClose = prop => (event, reason) => {
-    if (reason === 'clickaway') return;
-    setDel({ ...del, [prop]: false });
-  };
-
-  const deleteExecom = () => {
-    // console.log(hostname + '/api/execom/end/'+ ecats.compsoc);
-    axios
-      .post(hostname + '/api/execom/end/', ecats.compsoc, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + localStorage.getItem('atoken'),
-        },
-    })
-    .then(response => {
-      if (response.data.ok === true) setDel({ ...del, success: true });
-      else setDel({ ...del, error: true });
-    })
-    .then(() => {
-      window.location.reload();
-    })
-    .catch(error => {
-      console.error(error.response);
-      setDel({ ...del, error: true });
-    });
-  }
 
   return (
     <div className={classes.root}>
@@ -162,50 +111,13 @@ export default function CSSocietyPage(props) {
           </>
         )}
         <Paper className={classes.paper}>
-          <Typography variant="h3">Executive Committee
-          {loggedIn && (
-            <>
-          <IconButton component="span" onClick={deleteExecom}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
-          <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
-          <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
-          </svg>
-          </IconButton>
-          </>
-          )}
-          </Typography>
+          <Typography variant="h3">Executive Committee</Typography>
           <br />
-          <Grid container spacing={2} justify="center">
-            {execom.compsoc.map(member => (
-              <Grid item xs={12} md={4}>
-                <Avatar name={member.name} position={member.position} src={member.image} />
-              </Grid>
-            ))}
-          </Grid>
+          <SocietyExecom sid = {ecats.compsoc} exelist = {execom.compsoc} />
         </Paper>
-        {loggedIn && (
-        <>
-        <Tooltip title="Add Execom" aria-label="add-execom-tooltip">
-            <Fab onClick={handleDialogOpen} aria-label="addExecom" className={classes.fab}>
-              <Add />
-            </Fab>
-          </Tooltip>
-          <AddExecomDialog onClose={handleDialogClose} aria-label="add-execom-dialog" open={dialog} sid={ecats.compsoc}/>
-        </>
-      )}
         <AlumniAccordions members={alumni.compsoc} color="rgb(80 161 99)" />
         <br />
       </Container>
-      <Snackbar open={del.error} autoHideDuration={6000} onClose={handleClose('error')}>
-        <Alert elevation={6} variant="filled" onClose={handleClose('error')} severity="error">
-          An error occurred while deleting, please try again
-        </Alert>
-      </Snackbar>
-      <Snackbar open={del.success} autoHideDuration={6000} onClose={handleClose('success')}>
-        <Alert elevation={6} variant="filled" onClose={handleClose('success')} severity="success">
-          Successfully ended tenure of current execom
-        </Alert>
-      </Snackbar>
     </div>
   );
 }
