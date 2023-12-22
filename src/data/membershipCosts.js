0eaae4bb5a5ce0b18d28984c216020c64bb0d437
SundarakrishnanN <sundar050804@gmail.com>
2023-12-22 23:06:37 +0530

    Updated the prices and google form

--- src/data/membershipCosts.js
+++ src/data/membershipCosts.js
@@ -1,4 +1,4 @@
-export const exchangeRate = 77;
+export const exchangeRate = 1;
 /**
  * Membership costs. Cost is by default 0, notes by default is taken as empty.
  * Every Year IEEE decides a fixed doller exchange rate. This year(2023) it is 1 $= 77 rupees. we take it and convert it to INR, contact MDC Chair for more details.
@@ -11,42 +11,42 @@ export const exchangeRate = 77;
 export const costs = [
   {
     name: 'Student Membership',
-    newCost: 3.5,
-    renewalCost: 14,
+    newCost: 1372,
+    renewalCost: 1372,
     // discountedCost: 27 / 2,
     //notes: "FUTURE50 (50% off)",
-    notes: 'Valid till Dec 31st',
+   // notes: 'Valid till Dec 31st',
   },
 
   {
     name: 'Antennas and Propagation Society (APS)',
-    newCost: 8,
-    renewalCost: 8,
+    newCost: 93,
+    renewalCost: 93,
   },
 
-  { name: 'Circuits and Systems (CAS)', newCost: 5.5, renewalCost: 5.5 },
+  { name: 'Circuits and Systems (CAS)', newCost: 0, renewalCost: 0 },
 
-  { name: 'Communications Society', newCost: 0.5, renewalCost: 0.5 },
+  { name: 'Communications Society', newCost: 47.5, renewalCost: 47.5 },
 
   {
     name: 'Computer Society',
-    newCost: 4,
-    renewalCost: 4,
+    newCost: 392,
+    renewalCost: 392,
     // discountedCost: 4,
     // notes: "CS202250 (50% off)",
   },
 
   {
     name: 'Power & Energy Society (PES)',
-    newCost: 0,
-    renewalCost: 7.5,
+    newCost: 47.5,
+    renewalCost: 47.5,
     // discountedCost: 0,
     // notes: 'Free for new members!',
   },
 
-  { name: 'Robotics and Automation (RAS)', newCost: 5, renewalCost: 5 },
+  { name: 'Robotics and Automation (RAS)', newCost: 465, renewalCost: 465 },
 
-  { name: 'Signal Processing Society (SPS)', newCost: 0.5, renewalCost: 0.5 },
+  { name: 'Signal Processing Society (SPS)', newCost: 47.5, renewalCost: 47.5 },
   
   { name: 'Sensors Council', newCost: 0, renewalCost: 0 },
   
