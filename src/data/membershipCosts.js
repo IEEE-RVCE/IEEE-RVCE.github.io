e73a2797ee43e66a0423c3bf563ba826f5bba29a
Tejas Pattar <tejaspattar03@gmail.com>
2024-01-27 23:20:46 +0530

    Price Changes

--- src/data/membershipCosts.js
+++ src/data/membershipCosts.js
@@ -19,6 +19,12 @@ export const costs = [
   },
 
   {
+    name: ' Aerospace and Electronic Systems Society(AESS))',
+    newCost: 465,
+    renewalCost: 465,
+  },
+
+  {
     name: 'Antennas and Propagation Society (APS)',
     newCost: 93,
     renewalCost: 93,
@@ -26,10 +32,10 @@ export const costs = [
 
   { name: 'Circuits and Systems (CAS)', newCost: 0, renewalCost: 0 },
 
-  { name: 'Communications Society', newCost: 47.5, renewalCost: 47.5 },
+  { name: 'Communications Society(ComSoc)', newCost: 47.5, renewalCost: 47.5 },
 
   {
-    name: 'Computer Society',
+    name: 'Computer Society(CompSoc)',
     newCost: 392,
     renewalCost: 392,
     // discountedCost: 4,
@@ -37,20 +43,20 @@ export const costs = [
   },
 
   {
-    name: 'Power & Energy Society (PES)',
-    newCost: 47.5,
-    renewalCost: 47.5,
+    name: 'Microwave Theory and Technology Society(MTTS)',
+    newCost: 93,
+    renewalCost: 93,
     // discountedCost: 0,
     // notes: 'Free for new members!',
   },
 
-  { name: 'Robotics and Automation (RAS)', newCost: 465, renewalCost: 465 },
+  { name: 'Power and Energy Society(PES)', newCost:  47.5, renewalCost:  47.5},
 
-  { name: 'Signal Processing Society (SPS)', newCost: 47.5, renewalCost: 47.5 },
+  { name: ' Robotics and Automation Society(RAS)', newCost: 465, renewalCost: 465 }, 
   
-  { name: 'Sensors Council', newCost: 0, renewalCost: 0 },
+  { name: 'Signal Processing Society(SPS)', newCost: 47.5, renewalCost:  47.5},
   
-  { name: 'SIGHT', newCost: 0, renewalCost: 0 },
+  { name: '  Affinity Group-SIGHT', newCost: 0, renewalCost: 0 },
 
-  { name: 'Women in Engineering (WIE)', newCost: 0, renewalCost: 0 },
+  { name: 'Affinity Group-Women in Engineering(WIE)', newCost: 0, renewalCost: 0 },
 ];
